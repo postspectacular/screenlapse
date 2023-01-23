d3b2d92a49ae53654e0a1feed31bd4368e09add5
Karsten Schmidt <k@thi.ng>
2023-01-23 08:44:08 +0100

    refactor: switch to module imports

--- screenlapse.js
+++ screenlapse.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-const screenshot = require("screenshot-desktop");
-const Z4 = require("@thi.ng/strings").padLeft(4, "0");
+import screenshot from "screenshot-desktop";
+import { Z4 } from "@thi.ng/strings";
 
 let id = 0;
 let delay = 500;
 
 switch (process.argv.length - 2) {
-    case 2:
-        id = parseInt(process.argv[3]);
-    case 1:
-        delay = parseInt(process.argv[2]);
-        break;
-    default:
+	case 2:
+		id = parseInt(process.argv[3]);
+	case 1:
+		delay = parseInt(process.argv[2]);
+		break;
+	default:
 }
 
 console.log(`recording from id=${id} @ ${delay}ms...`);
